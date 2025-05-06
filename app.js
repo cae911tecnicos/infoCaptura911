46c594de4c6cbf70636c0fa6be596b24dea0210d
cae911tecnicos <76053145+cae911tecnicos@users.noreply.github.com>
2025-05-06 13:08:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const imagenes = [
-  "https://github.com/cae911tecnicos/infoCaptura911/blob/main/imagenes/florespi6-5-25.PNG",
+  "https://github.com/cae911tecnicos/infoCaptura911/blob/685049537421916fc77f3a6ba114ac3c36207fc1/imagenes/florespi6-5-25.PNG",
 ];
 
 function loadCarousel() {
