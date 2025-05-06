9b7957df6c3a9a2dac3f5e70d729282f72be2d6b
cae911tecnicos <76053145+cae911tecnicos@users.noreply.github.com>
2025-05-06 13:33:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 const config = {
     autoSlideInterval: 5000, // 5 segundos
     imageUrls: [
-        "https://raw.githubusercontent.com/cae911tecnicos/infoCaptura911/main/imagenes/florespi6-5-25.PNG"
+        "https://raw.githubusercontent.com/cae911tecnicos/infoCaptura911/main/imagenes/florespi6-5-25.PNG",
+        "https://raw.githubusercontent.com/cae911tecnicos/infoCaptura911/main/imagenes/maquiavelolj6-5-25.PNG",
+        "https://raw.githubusercontent.com/cae911tecnicos/infoCaptura911/main/imagenes/monsonja6-5-25.PNG"
     ]
 };
 
